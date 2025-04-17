8fd467e8c63b43e1bc26c2e84dcdf6472fcbaf69
Daniel Martin <danielwmartin1@gmail.com>
2025-04-17 07:05:59 -0400

    transfer project from old repo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  SUPABASE_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmaG1wcHNrbHZnenpxbGlwa2tpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2MTg1NzcsImV4cCI6MjA1MzE5NDU3N30.lz4jclcj2DsR5IzLC7SenW-Wp-MybY4oR1jCFJSl2wM',
+  SUPABASE_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.SUPABASE_KEY.lz4jclcj2DsR5IzLC7SenW-Wp-MybY4oR1jCFJSl2wM',
 };
